e2415316e0142b9e0056146a83833d17421e0eb8
krop24 <umutbaiev20022002@gmail.com>
2023-09-05 20:54:49 +0600

    add: header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,11 @@ import React from 'react'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="ccntainer">
+            {children}
+        </div>
+      </body>
     </html>
   )
 }
