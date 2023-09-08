70c4bf1aa83b13fed1d021bf9652bd5f7ca7e9ee
Dmitry Petrov <dimaninc@gmail.com>
2023-09-08 17:36:43 +0300

    svg example

--- src/icons/index.tsx
+++ src/icons/index.tsx
@@ -1,34 +1,7 @@
 import React from 'react';
+import HomeIcon from '../../public/icons/menu/home.svg';
 
-export const HomeIcon: React.FC = props => {
-  return (
-    <svg {...props} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M2 22H22"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeMiterlimit="10"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M2.94922 22L2.99922 9.96999C2.99922 9.35999 3.28922 8.78004 3.76922 8.40004L10.7692 2.95003C11.4892 2.39003 12.4992 2.39003 13.2292 2.95003L20.2292 8.39003C20.7192 8.77003 20.9992 9.34999 20.9992 9.96999V22"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeMiterlimit="10"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M15.5 11H8.5C7.67 11 7 11.67 7 12.5V22H17V12.5C17 11.67 16.33 11 15.5 11Z"
-        stroke="currentColor"
-        strokeWidth="1.5"
-        strokeMiterlimit="10"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-};
+export { HomeIcon };
 
 export const FolderIcon: React.FC = props => {
   return (
