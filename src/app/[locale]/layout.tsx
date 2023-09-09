9b72767195f3bf07e643a0ae272a38b06583baea
krop24 <umutbaiev20022002@gmail.com>
2023-09-09 22:32:58 +0600

    fix: scss config

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import '@/styles/variables.scss';
-import '@/styles/globals.scss';
+import '@/app/[locale]/variables.scss';
+import '@/app/[locale]/globals.scss';
 
 import { Inter, Poppins } from 'next/font/google';
 import { notFound } from 'next/navigation';
