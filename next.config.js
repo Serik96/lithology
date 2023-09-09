0b26aa08933b2057bff5e5c1a32223a349501c97
krop24 <umutbaiev20022002@gmail.com>
2023-09-09 22:10:04 +0600

    fix: scss config

--- next.config.js
+++ next.config.js
@@ -4,8 +4,22 @@ const path = require('path');
 const withNextIntl = require('next-intl/plugin')('./src/i18n.ts');
 
 const nextConfig = {
-  sassOptions: {
-    includePaths: [path.join(__dirname, 'src')],
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.scss$/,
+      use: [
+        'style-loader',
+        'css-loader',
+        {
+          loader: 'sass-loader',
+          options: {
+            includePaths: [path.join(__dirname, 'src')],
+          },
+        },
+      ],
+    });
+
+    return config;
   },
 };
 
