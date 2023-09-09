92269197383d44b07e414dab153ca87b7ed2908d
krop24 <umutbaiev20022002@gmail.com>
2023-09-09 22:34:17 +0600

    fix: loader styles

--- src/components/UI/loader/index.tsx
+++ src/components/UI/loader/index.tsx
@@ -2,10 +2,10 @@ const Loader = () => {
   return (
     <div className="loader">
       <h1 className='loader__heading'>
-
+        Loading...
       </h1>
       <h4 className='loader__desctiption'>
-
+        Please wait
       </h4>
     </div>
   )
