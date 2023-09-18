010450b34244731d30cc708f6da7817e94ec899e
krop24 <umutbaiev20022002@gmail.com>
2023-09-18 18:58:31 +0600

    add: main page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,25 @@
 import { useTranslations } from 'next-intl';
+import s from './main.module.scss';
+import Image from 'next/image';
 
 const Main = () => {
-  const t = useTranslations('home');
+  const t = useTranslations();
 
-  return <div>{t('title')}</div>;
+  return (
+    <div className={s.main}>
+      <div className={s.wrapper}>
+        <h1 className={s.heading}>{t('main.title')}</h1>
+        <p className={s.description}>{t('main.description')}</p>
+        <Image
+          className={s.playImg}
+          width={220}
+          height={220}
+          alt={t('main.play-img-label')}
+          src="/icons/play.svg"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default Main;
