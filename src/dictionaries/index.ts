3adbc5c7937fa298067ee692c19d3e750841ffbd
Бахтияр Умутбаев <bu@init.kz>
2023-10-10 15:20:03 +0600

    fix: not found error

--- src/dictionaries/index.ts
+++ src/dictionaries/index.ts
@@ -6,3 +6,7 @@ import ru from './ru.json';
  */
 
 export const dictionaries = { en, ru };
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+export const getDictionary = async (locale: string) => dictionaries[locale];
