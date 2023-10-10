b5e1a928c0dbb7a948a1c14fdd3b964b1417f2a2
Бахтияр Умутбаев <bu@init.kz>
2023-10-10 15:25:28 +0600

    fix: pr

--- src/dictionaries/index.ts
+++ src/dictionaries/index.ts
@@ -6,7 +6,3 @@ import ru from './ru.json';
  */
 
 export const dictionaries = { en, ru };
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-export const getDictionary = async (locale: string) => dictionaries[locale];
