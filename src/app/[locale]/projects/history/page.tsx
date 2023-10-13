4ed049f7be85632872a0cc1fe42368201bf32df6
Serik <s.kappar@aster.kz>
2023-10-13 15:25:14 +0600

    add: search-input

--- src/app/[locale]/projects/history/page.tsx
+++ src/app/[locale]/projects/history/page.tsx
@@ -1,19 +1,32 @@
 'use client';
 
 import { useTranslations } from 'next-intl';
+import { useEffect, useState } from 'react';
 import { Breadcrumbs } from '@/components/ui/breadcrumbs';
+import { SeacrhType } from '@/components/ui/search-input';
+import { TUser } from '@/types';
 import { historyBreadcrumbs } from './const';
 import s from './history.module.scss';
 
 const Created = () => {
   const t = useTranslations();
+  const [data, setData] = useState<TUser[]>();
+
+  const getUsers = () => {
+    setData(data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
 
   return (
     <>
       <Breadcrumbs navLinks={historyBreadcrumbs} />
       <div className={s.historyProject}>
         <div className={s.wrapper}>
-          <h1 className={s.heading}>{t('projects.history-report')}</h1>
+          <h1 className={s.heading}>{t('projects.history-report')} 15</h1>
+          <SeacrhType></SeacrhType>
         </div>
       </div>
     </>
